3995143369d3c0d08a4cb74625b8e97fc96257f6
Sara <sara.szajewska@gmail.com>
2017-05-30 12:45:38 +0200

    test input element

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from "enzyme";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+test("Test input-element", () => {
+  let wrapper = shallow(<App/>);
+  const greeting = <input placeholder="name"/>;
+  expect(wrapper.contains(greeting)).toBe(true)
+                        
+});
+
